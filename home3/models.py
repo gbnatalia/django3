245e4e020fe22a7afecf8bd0b05cec27132ac2fb
1 <1>
2023-09-18 17:59:32 +0300

    ДЗ5

--- home3/models.py
+++ home3/models.py
@@ -10,7 +10,7 @@ class Client(models.Model):
 		○ дата регистрации клиента
     '''
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(default='email@mail.com')
     tel = models.CharField(max_length=11)
     adress = models.CharField(max_length=300)
     reg_date = models.DateField(auto_now_add=True)
@@ -29,11 +29,11 @@ class Product(models.Model):
 		○ дата добавления товара
     '''
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(default='', blank=True, max_length=1000)
+    price = models.DecimalField(default='0.00', max_digits=10, decimal_places=2)
     count = models.IntegerField(default=0)
     add_date = models.DateField(auto_now_add=True)
-    image = models.ImageField(null=True)
+    image = models.ImageField(default="", null=True)
 
     def __str__(self):
         return f'{self.name} {self.description} {self.price} {self.count} {self.add_date}'
